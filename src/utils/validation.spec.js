8fdefccf81582612343acc84007582f5110c3e80
Alexander Babai <aliaksandr.babai@gmail.com>
2019-12-24 15:33:14 +0300

    test(validation): improve tests

--- src/utils/validation.spec.js
+++ src/utils/validation.spec.js
@@ -1,49 +1,67 @@
 const { describe, it } = require('mocha')
-const { expect, assert } = require('chai')
+const { expect, assert, use } = require('chai')
+const { spy } = require('sinon')
+const sinonChai = require('sinon-chai')
+
+use(sinonChai)
 
 const { createValidator } = require('./validation')
 
 describe('Validation module', () => {
     describe('#createValidator(predicate, error?)', () => {
-        it('Should return object', () => {
-            const predicate = () => true
-            const validator = createValidator(predicate)
-            expect(validator).to.exist
-            const input = {}
-            const result = validator(input)
-            expect(result).equals(input)
-        })
-        it('Should return validator with default error', () => {
-            const predicate = () => false
-            const validator = createValidator(predicate)
-            expect(validator).to.exist
-            assert.throws(() => validator({}), 'Validation failed!')
-        })
-        describe('Should return validator with custom error', () => {
-            it('pass error supplier', () => {
-                const predicate = () => false
-                const errorMessage = 'ERROR'
-                const validator = createValidator(predicate, () => new Error(errorMessage))
-                expect(validator).to.exist
-                assert.throws(() => validator({}), errorMessage)
+        describe('Validator creation', () => {
+            it('Should return validator function', () => {
+                const predicate = () => true
+                const validator = createValidator(predicate)
+                expect(validator)
+                    .to.exist
+                    .and
+                    .to.be.a('function')
             })
-            it('pass message', () => {
-                const predicate = () => false
-                const errorMessage = 'ERROR'
-                const validator = createValidator(predicate, errorMessage)
-                expect(validator).to.exist
-                assert.throws(() => validator({}), errorMessage)
-            })
-            it('pass error object', () => {
-                const predicate = () => false
-                const errorMessage = 'ERROR'
-                const validator = createValidator(predicate, new Error(errorMessage))
-                expect(validator).to.exist
-                assert.throws(() => validator({}), errorMessage)
+            it('Should throw an Error - invalid predicate', () => {
+                assert.throws(() => createValidator(), 'Invalid predicate!')
             })
         })
-        it('Should throw an Error - invalid predicate', () => {
-            assert.throws(() => createValidator(), 'Invalid predicate!')
+        describe('Validation logic', () => {
+            it('Should pass validation', () => {
+                const object = {}
+                const predicate = spy(() => true)
+                const validator = createValidator(predicate)
+                validator(object)
+                expect(predicate).to.be.calledOnceWith(object)
+            })
+            it('Should fail validation with default error', () => {
+                const object = {}
+                const predicate = spy(() => false)
+                const validator = createValidator(predicate)
+                assert.throws(() => validator(object), 'Validation failed!')
+            })
+            describe('Should fail validation custom error', () => {
+                it('pass error supplier', () => {
+                    const object = {}
+                    const predicate = spy(() => false)
+                    const errorMessage = 'ERROR'
+                    const validator = createValidator(predicate, () => new Error(errorMessage))
+                    assert.throws(() => validator(object), errorMessage)
+                    expect(predicate).to.be.calledOnceWith(object)
+                })
+                it('pass message', () => {
+                    const object = {}
+                    const predicate = spy(() => false)
+                    const errorMessage = 'ERROR'
+                    const validator = createValidator(predicate, errorMessage)
+                    assert.throws(() => validator(object), errorMessage)
+                    expect(predicate).to.be.calledOnceWith(object)
+                })
+                it('pass error object', () => {
+                    const object = {}
+                    const predicate = spy(() => false)
+                    const errorMessage = 'ERROR'
+                    const validator = createValidator(predicate, new Error(errorMessage))
+                    assert.throws(() => validator(object), errorMessage)
+                    expect(predicate).to.be.calledOnceWith(object)
+                })
+            })
         })
     })
 })
